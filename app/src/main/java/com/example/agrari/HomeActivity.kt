7713ce1ce80401b54eece4bb4d5f8ff55b7c237f
natalia-chaparro <natalia-chaparro@nataliachaparro-TM1701>
2022-08-30 18:40:29 -0500

    Creación pantalla ajustes

---
+++ app/src/main/java/com/example/agrari/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.agrari
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.agrari.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
