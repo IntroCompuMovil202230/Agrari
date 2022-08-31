2061a0c7162fa7ae5c3b2022988c19e7e7a908d7
MarcoAValencia <marco_a_valencia@hotmail.com>
2022-08-30 21:21:05 -0500

    temporalcommit

---
+++ app/src/main/java/com/example/agrari/HomeVendedor.kt
@@ -0,0 +1,11 @@
+package com.example.agrari
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeVendedor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_vendedor)
+    }
+}
---
+++ app/src/main/java/com/example/agrari/Publicacion.kt
@@ -0,0 +1,11 @@
+package com.example.agrari
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Publicacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publicacion)
+    }
+}
